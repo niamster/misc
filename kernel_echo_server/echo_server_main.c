68a7a26c72a98d7b2a9704599c74444f2692118a
Dmytro Milinevskyy <milinevskyy@gmail.com>
2010-06-21 23:12:28 +0300

    [kernel echo server] receive UDP dgrams

--- kernel_echo_server/echo_server_main.c
+++ kernel_echo_server/echo_server_main.c
@@ -31,8 +31,13 @@ static char *echo_server_ip = "0.0.0.0";
 module_param(echo_server_ip, charp, 0644);
 MODULE_PARM_DESC(echo_server_ip, "Echo server ip address");
 
+static char *echo_server_prot = "udp";
+module_param(echo_server_prot, charp, 0644);
+MODULE_PARM_DESC(echo_server_prot, "Echo server protocol(udp/tcp)");
+
 static struct socket *echo_server_socket;
-struct task_struct *echo_server_thread;
+static struct task_struct *echo_server_thread;
+static int echo_server_tcp;
 
 static void echo_server_data_ready(struct sock *sk, int bytes)
 {
@@ -40,7 +45,7 @@ static void echo_server_data_ready(struct sock *sk, int bytes)
     wake_up_process(echo_server_thread);
 }
 
-static int echo_server(void *data)
+static int echo_server_tcp_thread(void *data)
 {
     static struct socket *in_socket;
     struct sockaddr_in remote_addr;
@@ -54,6 +59,9 @@ static int echo_server(void *data)
         /* No pending events. Let's sleep.
          * Incoming connections and data will wake us up. */
         schedule();
+
+        if (kthread_should_stop())
+            break;
 
         set_current_state(TASK_RUNNING);
 
@@ -94,12 +102,82 @@ static int echo_server(void *data)
                 set_current_state(TASK_INTERRUPTIBLE);
                 schedule();
 
+                if (kthread_should_stop())
+                    break;
+
                 set_current_state(TASK_RUNNING);
                 goto retry_rcv;
             }
 
             kernel_sock_shutdown(in_socket, SHUT_RDWR);
             sock_release(in_socket);
+        }
+    }
+
+	return 0;
+}
+
+static int echo_server_udp_thread(void *data)
+{
+    /* static struct socket *in_socket; */
+    struct sockaddr_in remote_addr;
+
+    int remote_addr_size;
+    size_t msg_len;
+    struct kvec rx_iov;
+    struct msghdr rx_msg;
+    int n;
+
+    u8 msg_buf[32];
+
+    printk(KERN_INFO "echo server: ready for accepting connections\n");
+
+    while (!kthread_should_stop()) {
+		set_current_state(TASK_INTERRUPTIBLE);
+        /* No pending events. Let's sleep.
+         * Incoming connections and data will wake us up. */
+        schedule();
+
+        if (kthread_should_stop())
+            break;
+
+        set_current_state(TASK_RUNNING);
+
+        remote_addr_size = sizeof(remote_addr);
+        msg_len = sizeof(msg_buf);
+        rx_iov.iov_base = msg_buf, rx_iov.iov_len = msg_len;
+        memset(&rx_msg, 0x0, sizeof(rx_msg));
+        rx_msg.msg_flags = MSG_DONTWAIT;
+
+        printk(KERN_INFO "echo server: accepted connection\n");
+        if (kernel_getsockname(echo_server_socket, (struct sockaddr *)&remote_addr,
+                        &remote_addr_size) >= 0) {
+            u8 ip[sizeof("255.255.255.255")];
+            u32 src = ntohl(remote_addr.sin_addr.s_addr);
+            sprintf(ip, "%u.%u.%u.%u",
+                    (src>>24)&0xFF,
+                    (src>>16)&0xFF,
+                    (src>> 8)&0xFF,
+                    (src>> 0)&0xFF);
+            printk(KERN_INFO "echo server: remote peer ip %s, port %d\n",
+                    ip, ntohs(remote_addr.sin_port));
+        }
+
+        memset(msg_buf, 0x0, msg_len);
+      retry_rcv:
+        if ((n = kernel_recvmsg(echo_server_socket, &rx_msg, &rx_iov, 1, msg_len, rx_msg.msg_flags)) >= 0) {
+            msg_buf[n] = 0x0;
+            printk(KERN_INFO "echo server: received %d bytes message %s\n",
+                    n, msg_buf);
+        } else {
+            set_current_state(TASK_INTERRUPTIBLE);
+            schedule();
+
+            if (kthread_should_stop())
+                break;
+
+            set_current_state(TASK_RUNNING);
+            goto retry_rcv;
         }
     }
 
@@ -157,18 +235,27 @@ static int __init echo_server_init(void)
     u32 ip_addr;
     int ret;
 
+    if (!strcmp(echo_server_prot, "udp")) {
+        echo_server_tcp = 0;
+    } else if (!strcmp(echo_server_prot, "tcp")) {
+        echo_server_tcp = 1;
+    } else {
+        printk(KERN_ERR "echo server: wrong protocol: %s\n", echo_server_prot);
+        return -EINVAL;
+    }
+
     if (!inet_pton(echo_server_ip, &ip_addr)) {
         printk(KERN_ERR "echo server: wrong ip address %s\n", echo_server_ip);
         return -EINVAL;
     }
 
-    echo_server_thread = kthread_create(echo_server, NULL, "echod");
+    echo_server_thread = kthread_create(echo_server_tcp?echo_server_tcp_thread:echo_server_udp_thread, NULL, "echod");
 	if (IS_ERR(echo_server_thread)) {
         printk(KERN_ERR "echo server: unable to create thread\n");
 		return PTR_ERR(echo_server_thread);
     }
 
-	ret = sock_create_kern(AF_INET, SOCK_STREAM, 0, &echo_server_socket);
+	ret = sock_create_kern(AF_INET, echo_server_tcp?SOCK_STREAM:SOCK_DGRAM, 0, &echo_server_socket);
 	if (ret < 0) {
         printk(KERN_ERR "echo server: unable to create socket.\n");
         goto exit_stop_thread;
@@ -187,16 +274,18 @@ static int __init echo_server_init(void)
         goto exit_sock_release;
 	}
 
-    ret = kernel_listen(echo_server_socket, 10);
-	if (ret < 0) {
-        printk(KERN_ERR "echo server: unable to listen socket.\n");
-        goto exit_sock_release;
-	}
+    if (echo_server_tcp) {
+        ret = kernel_listen(echo_server_socket, 10);
+        if (ret < 0) {
+            printk(KERN_ERR "echo server: unable to listen socket.\n");
+            goto exit_sock_release;
+        }
+    }
 
     wake_up_process(echo_server_thread);
 
-    printk(KERN_INFO "echo server: listening for connections on ip %s, port %d\n",
-            echo_server_ip, echo_server_port);
+    printk(KERN_INFO "echo server: listening for %s connections on ip %s, port %d\n",
+            echo_server_prot, echo_server_ip, echo_server_port);
 
     return 0;
 
