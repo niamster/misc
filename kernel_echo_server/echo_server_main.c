d9dddea6030d627152f43ea7fbc4c343a9033c05
niam <niam@niam-desktop.(none)>
2010-06-22 11:42:06 +0300

    [kernel echo server] UDP: do not get socket info from RX socket

--- kernel_echo_server/echo_server_main.c
+++ kernel_echo_server/echo_server_main.c
@@ -47,7 +47,7 @@ static void echo_server_data_ready(struct sock *sk, int bytes)
 
 static int echo_server_tcp_thread(void *data)
 {
-    static struct socket *in_socket;
+    struct socket *in_socket;
     struct sockaddr_in remote_addr;
 
     u8 msg_buf[32];
@@ -66,13 +66,14 @@ static int echo_server_tcp_thread(void *data)
         set_current_state(TASK_RUNNING);
 
         if (kernel_accept(echo_server_socket, &in_socket, O_NONBLOCK) >= 0) {
-            int remote_addr_size = sizeof(remote_addr);
+            int remote_addr_size = 0;
             size_t msg_len = sizeof(msg_buf);
             struct kvec rx_iov = {msg_buf, msg_len};
             struct msghdr rx_msg = { .msg_flags = MSG_DONTWAIT };
             int n;
 
             printk(KERN_INFO "echo server: accepted connection\n");
+
             if (kernel_getpeername(in_socket, (struct sockaddr *)&remote_addr,
                             &remote_addr_size) >= 0) {
                 u8 ip[sizeof("255.255.255.255")];
@@ -119,7 +120,6 @@ static int echo_server_tcp_thread(void *data)
 
 static int echo_server_udp_thread(void *data)
 {
-    /* static struct socket *in_socket; */
     struct sockaddr_in remote_addr;
 
     int remote_addr_size;
@@ -143,32 +143,58 @@ static int echo_server_udp_thread(void *data)
 
         set_current_state(TASK_RUNNING);
 
-        remote_addr_size = sizeof(remote_addr);
         msg_len = sizeof(msg_buf);
         rx_iov.iov_base = msg_buf, rx_iov.iov_len = msg_len;
         memset(&rx_msg, 0x0, sizeof(rx_msg));
         rx_msg.msg_flags = MSG_DONTWAIT;
 
-        printk(KERN_INFO "echo server: accepted connection\n");
-        if (kernel_getsockname(echo_server_socket, (struct sockaddr *)&remote_addr,
-                        &remote_addr_size) >= 0) {
-            u8 ip[sizeof("255.255.255.255")];
-            u32 src = ntohl(remote_addr.sin_addr.s_addr);
-            sprintf(ip, "%u.%u.%u.%u",
-                    (src>>24)&0xFF,
-                    (src>>16)&0xFF,
-                    (src>> 8)&0xFF,
-                    (src>> 0)&0xFF);
-            printk(KERN_INFO "echo server: remote peer ip %s, port %d\n",
-                    ip, ntohs(remote_addr.sin_port));
-        }
-
         memset(msg_buf, 0x0, msg_len);
       retry_rcv:
         if ((n = kernel_recvmsg(echo_server_socket, &rx_msg, &rx_iov, 1, msg_len, rx_msg.msg_flags)) >= 0) {
+            /* remote_addr_size = 0; */
+            /* if (rx_msg.msg_namelen && */
+            /*         kernel_getsockname(echo_server_socket, (struct sockaddr *)rx_msg.msg_name, */
+            /*                 &remote_addr_size) >= 0) { */
+            /*     u8 ip[sizeof("255.255.255.255")]; */
+            /*     u32 src = ntohl(remote_addr.sin_addr.s_addr); */
+            /*     sprintf(ip, "%u.%u.%u.%u", */
+            /*             (src>>24)&0xFF, */
+            /*             (src>>16)&0xFF, */
+            /*             (src>> 8)&0xFF, */
+            /*             (src>> 0)&0xFF); */
+            /*     printk(KERN_INFO "echo server: remote peer ip %s, port %d\n", */
+            /*             ip, ntohs(remote_addr.sin_port)); */
+            /* } */
+
             msg_buf[n] = 0x0;
             printk(KERN_INFO "echo server: received %d bytes message %s\n",
                     n, msg_buf);
+
+            /* if (remote_addr_size == sizeof(struct sockaddr_in)) { */
+            /*     struct socket *tx_socket; */
+            /*     struct kvec tx_iov = {msg_buf, n}; */
+            /*     struct msghdr tx_msg = { .msg_flags = MSG_WAITALL | MSG_EOR }; */
+
+            /*     if (sock_create_kern(AF_INET, SOCK_DGRAM, 0, &tx_socket) < 0) { */
+            /*         printk(KERN_ERR "echo server: unable to create socket for TX\n"); */
+            /*         goto unable_to_create_socket; */
+            /*     } */
+
+            /*     if (kernel_connect(tx_socket, (struct sockaddr *) &remote_addr, sizeof(remote_addr), 0) < 0) { */
+            /*         printk(KERN_ERR "echo server: unable to connect to remote peer\n"); */
+            /*         goto release_socket; */
+            /*     } */
+
+            /*     if (kernel_sendmsg(tx_socket, &tx_msg, &tx_iov, 1, n) < 0) */
+            /*         printk(KERN_ERR "echo server: failed to send message back\n"); */
+
+            /*   release_socket: */
+            /*     sock_release(tx_socket); */
+            /*   unable_to_create_socket: */
+            /*     ; */
+            /* } else { */
+            /*     printk(KERN_INFO "echo server: peer information is not known, not sending message back\n"); */
+            /* } */
         } else {
             set_current_state(TASK_INTERRUPTIBLE);
             schedule();
@@ -257,7 +283,7 @@ static int __init echo_server_init(void)
 
 	ret = sock_create_kern(AF_INET, echo_server_tcp?SOCK_STREAM:SOCK_DGRAM, 0, &echo_server_socket);
 	if (ret < 0) {
-        printk(KERN_ERR "echo server: unable to create socket.\n");
+        printk(KERN_ERR "echo server: unable to create socket\n");
         goto exit_stop_thread;
 	}
     sk = echo_server_socket->sk;
@@ -270,14 +296,14 @@ static int __init echo_server_init(void)
 
 	ret = kernel_bind(echo_server_socket, (struct sockaddr *) &addr, sizeof(addr));
 	if (ret < 0) {
-        printk(KERN_ERR "echo server: unable to bind socket.\n");
+        printk(KERN_ERR "echo server: unable to bind socket\n");
         goto exit_sock_release;
 	}
 
     if (echo_server_tcp) {
         ret = kernel_listen(echo_server_socket, 10);
         if (ret < 0) {
-            printk(KERN_ERR "echo server: unable to listen socket.\n");
+            printk(KERN_ERR "echo server: unable to listen socket\n");
             goto exit_sock_release;
         }
     }
